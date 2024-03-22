05994e378b63ece095f74bcc8eefb6702a693c7e
Patrice Tisserand <p.tisserand@gmail.com>
2024-03-22 10:27:06 +0100

    Add public key compute

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use lambdaworks_math::cyclic_group::IsGroup;
+use lambdaworks_math::elliptic_curve::{
+    short_weierstrass::curves::bls12_381::curve::BLS12381Curve, traits::IsEllipticCurve,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let g1 = BLS12381Curve::generator();
+    let sk = 0x6C616D6264617370_u64;
+    let pk = g1.operate_with_self(sk);
+    println!("Secret key: {:?}", sk);
+    println!("Public key: {:?}", pk.to_affine().x().to_hex());
 }
