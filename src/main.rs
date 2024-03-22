515a6fe17b5a209d21fb07792a5a7ca3f1f729f8
Patrice Tisserand <p.tisserand@gmail.com>
2024-03-22 10:53:23 +0100

    Add Goldilocks inverse of 2 computation

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use lambdaworks_math::cyclic_group::IsGroup;
 use lambdaworks_math::elliptic_curve::{
     short_weierstrass::curves::bls12_381::curve::BLS12381Curve, traits::IsEllipticCurve,
 };
+use lambdaworks_math::field::fields::u64_goldilocks_field::Goldilocks64Field;
+use lambdaworks_math::field::traits::IsField;
 
 fn main() {
     let g1 = BLS12381Curve::generator();
@@ -9,4 +11,6 @@ fn main() {
     let pk = g1.operate_with_self(sk);
     println!("Secret key: {:?}", sk);
     println!("Public key: {:?}", pk.to_affine().x().to_hex());
+    let inv_2 = Goldilocks64Field::inv(&2_u64).unwrap();
+    println!("Goldilocks inverse of 2: {:?}", inv_2)
 }
